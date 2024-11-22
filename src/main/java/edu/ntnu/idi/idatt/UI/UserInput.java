f2172e922320e6ffb464ac0d29ccd7a6def0fb00
MrAbiotic <harald.helland.velde@gmail.com>
2024-11-22 18:44:59 +0100

    Created methods to better handle user input

--- src/main/java/edu/ntnu/idi/idatt/UI/UserInput.java
+++ src/main/java/edu/ntnu/idi/idatt/UI/UserInput.java
@@ -4,7 +4,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class UserInput {
-  Scanner scanner = new Scanner(System.in);
+  private final Scanner scanner = new Scanner(System.in);
 
   /**
    * This class asks for the user input.
@@ -15,30 +15,74 @@ public class UserInput {
    * @return Returns the byte as the action of the user.
    * @throws IllegalArgumentException Raises exception if invalid input.
    */
-  public byte inputNumber(int max) throws IllegalArgumentException {
+  public byte inputByte(byte max) throws IllegalArgumentException, InputMismatchException {
     byte number = 0;
 
-
+    try {
+      number = scanner.nextByte();
+    }
+    catch (InputMismatchException e) {
+      scanner.next();
+      throw new InputMismatchException();
+    }
 
     return number;
   }
 
-  /**
-   * Checks if user-input is in av valid range. <br>
-   * <b>
-   *   Note that there are some values that will not return false.
-   *   These errors happens only to the second digit in "dd",
-   *   when the month is shorter than 31 days.
-   * </b>
-   *
-   * @param input Takes String as input
-   * @return boolean that indicates if the given date is valid
-   */
-  public boolean dateInput(String input) {
-    String regexCheckForDate = "([012][1-9]|3[01])\\.";
-    String regexCheckForMonth = "(0[1-9]|1[0-2])\\.";
-    String regexCheckForYear = "[0-9]{4}";
+  public double inputDouble(double max) throws IllegalArgumentException, InputMismatchException {
+    double number = 0;
 
-    return input.matches(regexCheckForDate + regexCheckForMonth + regexCheckForYear);
+    try {
+      number = scanner.nextDouble();
+    }
+    catch (InputMismatchException e) {
+      scanner.next();
+      throw new InputMismatchException();
+    }
+
+    return number;
+  }
+
+  public String inputString() throws IllegalArgumentException, InputMismatchException {
+    String string = "";
+
+    try {
+      string = scanner.nextLine();
+      if (string.length() < 2) {
+        System.out.println("Length" + string.length());
+        throw new IllegalArgumentException("Too short input. It must be at least 2 characters.");
+      }
+    }
+    catch (IllegalArgumentException e) {
+      scanner.next();
+      throw new IllegalArgumentException("Please enter a string with more than 2 characters.");
+    }
+    catch (InputMismatchException e) {
+      scanner.next();
+      throw new InputMismatchException("Please enter a valid string.");
+    }
+    return string;
+  }
+
+  public String inputDate() throws IllegalArgumentException, InputMismatchException {
+    String string = "";
+
+    try {
+      string = scanner.nextLine();
+      // Regex I made on my own after an introduction to logical Regex.
+      if (!string.matches("([012][1-9]|3[01])\\.(0[1-9]|1[0-2])\\.[0-9]{4}")) {
+        System.out.println("Does not conform the dd.MM.yyyy format.");
+        throw new IllegalArgumentException("Does not conform the dd.MM.yyyy format.");
+      }
+    }
+    catch (IllegalArgumentException e) {
+      scanner.next();
+      throw new IllegalArgumentException("Please enter a date on the format dd.MM.yyyy.");
+    }
+    catch (InputMismatchException e) {
+      scanner.next();
+      throw new InputMismatchException("Please enter a valid string.");
+    }
+    return string;
   }
 }
