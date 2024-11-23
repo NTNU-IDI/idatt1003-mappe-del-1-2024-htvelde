553ce6bf6bdc550a4f31d975fad7657a9b683439
MrAbiotic <harald.helland.velde@gmail.com>
2024-11-23 03:23:01 +0100

    Added dateToString-method

--- src/main/java/edu/ntnu/idi/idatt/Utils/Date.java
+++ src/main/java/edu/ntnu/idi/idatt/Utils/Date.java
@@ -4,9 +4,14 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class Date {
-  public LocalDate stringToDate(String date) {
+  public static LocalDate stringToDate(String date) {
     LocalDate parsedDate = LocalDate.parse(date, DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     return parsedDate;
+  }
+
+  public static String dateToString(LocalDate date) {
+    // Written by copilot (autocomplete)
+    return date.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
   }
 
   // Oldest date
